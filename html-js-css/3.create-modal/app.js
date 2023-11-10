1cb29f412a818a818548b922f0fa8c33931355e6
LuanB02 <103350520+LuanB02@users.noreply.github.com>
2023-11-10 11:55:13 +0700

    Add files via upload

--- html-js-css/3.create-modal/app.js
+++ html-js-css/3.create-modal/app.js
@@ -1,4 +1,16 @@
+const modal = document.querySelector('.modal')
+const openModalBtn = document.querySelector('.btn_modal')
+const iconCloseModal = document.querySelector('.modal-header i')
+const buttonCloseModal = document.querySelector('.modal-footer button')
 
-function myFunction() {
-    document.getElementById('demo').style.display='block'
+function toggleModal() {
+	modal.classList.toggle('hide1')
 }
+
+openModalBtn.addEventListener('click', toggleModal)
+iconCloseModal.addEventListener('click', toggleModal)
+buttonCloseModal.addEventListener('click', toggleModal)
+
+modal.addEventListener('click', (e) => {
+	if (e.target == e.currentTarget) toggleModal()
+})
