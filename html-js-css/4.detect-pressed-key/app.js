ba758ce519f1c98eab978baac56b4cdcc7a83d96
LuanB02 <103350520+LuanB02@users.noreply.github.com>
2023-11-10 11:55:44 +0700

    Add files via upload

--- html-js-css/4.detect-pressed-key/app.js
+++ html-js-css/4.detect-pressed-key/app.js
@@ -1,3 +1,21 @@
-document.addEventListener('keydown', (e) => {
-	// 	handle button action here
+const eKey = document.querySelector('.element.key p:last-child')
+const eLocation = document.querySelector('.element.location p:last-child')
+const eWhich = document.querySelector('.element.which p:last-child')
+const eCode = document.querySelector('.element.code p:last-child')
+const alert = document.querySelector('.alert')
+const container = document.querySelector('.container')
+const total = document.querySelector('.total')
+
+
+document.addEventListener('keydown', (e)=> {
+
+	eKey.innerText = e.key
+	eLocation.innerHTML = e.location
+	eWhich.innerHTML = e.which
+	eCode.innerHTML = e.code
+	total.innerHTML = e.which
+
+
+	alert.classList.add('hide')
+	container.classList.remove('hide')
 })
