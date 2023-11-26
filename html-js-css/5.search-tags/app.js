e63a02eaa2810ebf3c67676e81bc7e27b692a0a4
LuanB02 <103350520+LuanB02@users.noreply.github.com>
2023-11-26 16:28:57 +0700

    Add files via upload

--- html-js-css/5.search-tags/app.js
+++ html-js-css/5.search-tags/app.js
@@ -1,4 +1,41 @@
+let content = document.querySelector('.content');
+
+let input = document.querySelector('.content input');
+
+let removeAll = document.querySelector('.removeElement');
+
+let tags = ['Nodejs', 'Reactjs'];
+
+
 createTag()
-function createTag() {}
-function removeTag(element, index) {}
+function createTag() {
+    content.innerHTML = ''
+    for (let i = 0; i <tags.length; i++) {
+        const tag = tags[i]; 
+        content.innerHTML += `<li>
+                                ${tag}
+                                <i class="fa-solid fa-xmark" onclick="removeTag()"></i>
+                             </li>`
+
+    }
+    content.appendChild(input);
+    input.focus();
+}
+function removeTag(index) {
+    tags.splice(index,1);
+    createTag();
+}
 function addTag(e) {}
+
+input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        tags.push(input.value);
+        input.value = '';
+        createTag()
+    }   
+}) 
+removeAll.addEventListener('click', function() {
+    tags = [];
+    
+    createTag();
+})
